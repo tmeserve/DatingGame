2836400d03aea4fc1921e10c5b1a162f74e47f9a
tmeserve <31040347+tmeserve@users.noreply.github.com>
2017-10-02 09:21:17 -0500

    Add files via upload

--- MyDate.java
+++ MyDate.java
@@ -2,17 +2,13 @@ package datingGame;
 
 public class MyDate implements DateInterface
 {
-	static int year = 0;
-	static int month = 0;
-	static int day = 0;
-	static int dow = 0;
-	static String[] dows;
-	static String[] mont;
+	static int year;
+	static int month;
+	static int day;
+	static int doWeek;
+	static String[] dows = {"Sunday","Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"};
+	static String[] mont = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	
-	public MyDate()
-	{
-		
-	}
 	
 	public int getDay()
 	{
@@ -21,87 +17,12 @@ public class MyDate implements DateInterface
 
 	public int getDow()
 	{
-		if (dow == 0)
-		{
-			dows[0] = "Sunday";
-		}
-		else if (dow == 1)
-		{
-			dows[1] = "Monday";
-		}
-		else if (dow == 2)
-		{
-			dows[2] = "Tuesday";
-		}
-		else if (dow == 3)
-		{
-			dows[3] = "Wedensday";
-		}
-		else if (dow == 4)
-		{
-			dows[4] = "Thursday";
-		}
-		else if (dow == 5)
-		{
-			dows[5] = "Friday";
-		}
-		else if (dow == 6)
-		{
-			dows[6] = "Saturday";
-		}
-		return ;
+		
+		return doWeek;
 	}
 
 	public int getMonth()
 	{
-		if (month == 1)
-		{
-			mont[0] = "January";
-		}
-		else if (month == 2)
-		{
-			mont[1] = "February";
-		}
-		else if (month == 3)
-		{
-			mont[2] = "March";
-		}
-		else if (month == 4)
-		{
-			mont[3] = "April";
-		}
-		else if(month == 5)
-		{
-			mont[4] = "May";
-		}
-		else if (month == 6)
-		{
-			mont[5] = "June";
-		}
-		else if (month == 7)
-		{
-			mont[6] = "July";
-		}
-		else if (month == 8)
-		{
-			mont[7] = "August";
-		}
-		else if (month == 9)
-		{
-			mont[8] = "September";
-		}
-		else if (month == 10)
-		{
-			mont[9] = "October";
-		}
-		else if (month == 11)
-		{
-			mont[10] = "November";
-		}
-		else if (month == 12)
-		{
-			mont[11] = "December";
-		}
 		return month;
 	}
 	
@@ -112,29 +33,29 @@ public class MyDate implements DateInterface
 
 	public void set(int m, int d, int y, int dow)
 	{
-		m = month;
-		d = day;
-		y = year;
-		dow = getDow();
+		month = m;
+		day = d;
+		year = y;
+		doWeek = dow;
 	}
 
 	public void tomorrow()
 	{
 		day++;
-		dow++;
+		doWeek++;
 		if (day > 31)
 		{
 			month++;
 			day = 1;
 		}
-		else if (month > 12)
+		if (month > 11)
 		{
 			year++;
-			month = 1;
+			month = 0;
 		}
-		else if (dow > 6)
+		if (doWeek > 6)
 		{
-			dow = 0;
+			doWeek = 0;
 		}
 	}
 
@@ -150,6 +71,6 @@ public class MyDate implements DateInterface
 	
 	public String toString()
 	{
-		return dows[] + mont[] + day + year;
+		return dows[doWeek] + " " + mont[month] + " " + day + ", " + year;
 	}
 }
